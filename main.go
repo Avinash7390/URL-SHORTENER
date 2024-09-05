2aa641be89d85baa3670e9c3f7c3f6bf7a78cfd4
Avinash7390 <avinashk82495@gmail.com>
2024-09-05 17:08:37 +0530

    setting up the encoder function

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"time"
 )
@@ -12,6 +14,22 @@ type URL struct {
 	CreatedAt   time.Time `json:"created_at"`
 }
 
+var urlDB = make(map[string]URL)
+
+func GenerateShortUrl(OriginalURL string) string {
+	hasher := md5.New()
+
+	hasher.Write([]byte(OriginalURL))
+
+	data := hasher.Sum(nil)
+
+	hash := hex.EncodeToString(data)
+
+	return hash[:9]
+}
+
 func main() {
 	fmt.Println("Setting Up Project.")
+	shrt := GenerateShortUrl("https://helloworld.com")
+	fmt.Println(shrt)
 }
