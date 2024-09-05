dc6e1353d8bb3ae5225eca002e81e01705dd0b8b
Avinash7390 <avinashk82495@gmail.com>
2024-09-05 17:01:15 +0530

    structure to store the URLs

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type URL struct {
+	ID          string    `json:"id"`
+	OriginalURL string    `json:"original_url"`
+	ShortUrl    string    `json:"short_url"`
+	CreatedAt   time.Time `json:"created_at"`
+}
 
 func main() {
 	fmt.Println("Setting Up Project.")
